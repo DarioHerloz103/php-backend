38cb8e36d03420bc1affe6d8eba883d663469500
Dario Hernandez <darehloza@gmail.com>
2024-09-09 19:34:17 -0600

    Primeros pasos crud

---
+++ app/models/User.php
@@ -0,0 +1,17 @@
+<?php 
+Class User {
+  public $id;
+  public $nombre;
+  public $apaterno;
+  public $amaterno;
+  public $direccion;
+  public $telefono;
+  public $ciudad;
+  public $usuario;
+  public $password;
+  public $rol;
+  public $create_at;
+  public $updated_at;
+}
+
+?>
